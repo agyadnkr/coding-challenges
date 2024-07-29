e09d231c6ac17954a266cdc2d770718a3a567a8e
Brandon <brandnsyo@gmail.com>
2024-07-29 14:17:55 +0700

    Add Functions for main, routes, and interactor for connections and token

--- api/main.go
+++ api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"app/interactor"
+	"app/routes"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-	fmt.Println("test api")
+
+	e := echo.New()
+
+	data, err := interactor.InitDB()
+	if err != nil {
+		e.Logger.Fatal("Failed to connect to Database")
+	}
+
+	interactor.DB = data
+
+	routes.User(e)
+
+	e.Logger.Fatal(e.Start(":8080"))
+
 }
---
+++ api/routes/user.go
@@ -0,0 +1,15 @@
+package routes
+
+import "github.com/labstack/echo/v4"
+
+func User(e *echo.Echo) {
+
+	e.POST("Signup", controllers.Signup)
+
+	e.POST("Login", controllers.Login)
+
+	userGroup := e.Group("/User", middleware.CheckToken)
+
+	userGroup.POST("/Create", controllers.CreateUser)
+
+}
