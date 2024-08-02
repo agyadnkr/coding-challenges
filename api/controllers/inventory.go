e14b643a09b02a9b83aa744494faeed38c8a5c4e
Brandon <brandnsyo@gmail.com>
2024-08-02 14:34:37 +0700

    added the func inventory create

--- api/controllers/inventory.go
+++ api/controllers/inventory.go
@@ -1,8 +1,27 @@
 package controller
 
-import "github.com/labstack/echo/v4"
+import (
+	"app/model"
+	helpers "app/utility"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func CreateInventory(c echo.Context) error {
+	var inventory model.Inventory
 
-	return nil
+	if err := c.Bind(&inventory); err != nil {
+		return helpers.ReturnLog(c, http.StatusInternalServerError, "Error_bind_inventory")
+	}
+
+	if inventory.Wid == "" || inventory.Itmid == "" || inventory.Quantity == 0 {
+		return helpers.ReturnLog(c, http.StatusBadRequest, "Error_empty_fields")
+	}
+
+	if err := model.CreateInventory(inventory); err != nil {
+		return helpers.ReturnLog(c, http.StatusInternalServerError, "Error_create_inventory")
+	}
+
+	return c.JSON(http.StatusCreated, inventory)
 }
--- api/model/inventory.go
+++ api/model/inventory.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"errors"
 	"time"
 
 	"gorm.io/gorm"
@@ -11,7 +12,22 @@ type Inventory struct {
 	CreatedAt time.Time      `gorm:"column:created_at;autoCreateTime"`
 	UpdatedAt time.Time      `gorm:"column:updated_at;autoUpdateTime"`
 	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at"`
-	Itmid     uint           `gorm:"column:item_id" json:"item_id"`
+	Itmid     string         `gorm:"column:item_id" json:"item_id"`
 	Wid       string         `gorm:"column:warehouse_id" json:"warehouse_id"`
 	Quantity  int            `gorm:"column:quantity" json:"quantity"`
+}
+
+func CreateInventory(newInventory Inventory) error {
+	db := DB
+
+	var existingInventory Inventory
+	if err := db.Table("inventories").Where("item_id = ? AND warehouse_id = ?", newInventory.Itmid, newInventory.Wid).First(&existingInventory).Error; err == nil {
+		return errors.New("Inventory_for_this_item_and_warehouse_already_exists")
+	}
+
+	if err := db.Create(&newInventory).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
